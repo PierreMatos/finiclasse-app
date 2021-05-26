30ad837988022cf06af2f5ba6ae331447fc952e8
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2021-05-26 11:03:38 +0100

    Update CarModelResource.php

--- app/Http/Resources/CarModelResource.php
+++ app/Http/Resources/CarModelResource.php
@@ -9,7 +9,7 @@ class CarModelResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request  $requests
      * @return array
      */
     public function toArray($request)
