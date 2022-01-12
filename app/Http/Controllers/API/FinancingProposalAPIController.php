fc79efbfd5bd1b996436b4a979ad70d1e467e7ab
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-12 16:16:30 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -74,7 +74,7 @@ class FinancingProposalAPIController extends AppBaseController
 
             // add POS
             // dd($input); 
-        if ($input->hasFile('document')) {
+        if ($inputs->hasFile('document')) {
             $fileAdders = $newFinancingProposal->addMultipleMediaFromRequest(['document'])
                 ->each(function ($fileAdder) {
                     $fileAdder->toMediaCollection('financingproposal','s3');
