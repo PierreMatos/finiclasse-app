77d5f9a752ee775f5fcf96da42f9df91734208cd
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-12 15:57:06 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -56,7 +56,7 @@ class FinancingProposalAPIController extends AppBaseController
     {
 
         $inputs = $request->all();
-        // return($inputs);
+        return($inputs[0]);
         // return($inputs['Financings']);
         // DELTE RECORDS BEFORE INSERTING NEW
         // if($inputs){
