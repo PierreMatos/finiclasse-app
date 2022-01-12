f8b423698a35a2f8fd935d696f657f4bd61a0107
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-12 10:37:18 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -59,7 +59,7 @@ class FinancingProposalAPIController extends AppBaseController
 
         // dd($inputs);
         // DELTE RECORDS BEFORE INSERTING NEW
-        $deletedRows = FinancingProposal::where('proposal_id', $inputs[0]['proposal_id'])->delete();
+        // $deletedRows = FinancingProposal::where('proposal_id', $inputs[0]['proposal_id'])->delete();
 
         $items = collect();
 
