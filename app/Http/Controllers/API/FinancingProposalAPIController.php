010094eeaa516ba2c4713afd97f7a450fc43a208
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-12 12:16:01 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -62,26 +62,27 @@ class FinancingProposalAPIController extends AppBaseController
         // if($inputs){
         //     $deletedRows = FinancingProposal::where('proposal_id', $inputs[0]['proposal_id'])->delete();
         // }
+        $newFinancingProposal = $this->financingProposalRepository->create($inputs);
 
-        $items = collect();
+    //     $items = collect();
 
-        foreach ($inputs as $input){
+    //     foreach ($inputs as $input){
 
-            // ADD NEW FINANCINGS TO PROPOSAL
-            $newFinancingProposal = $this->financingProposalRepository->create($input);
+    //         // ADD NEW FINANCINGS TO PROPOSAL
+    //         $newFinancingProposal = $this->financingProposalRepository->create($input);
 
-            // add POS
-            // dd($input); 
-        if ($input->hasFile('document')) {
-            $fileAdders = $newFinancingProposal->addMultipleMediaFromRequest(['document'])
-                ->each(function ($fileAdder) {
-                    $fileAdder->toMediaCollection('financingproposal','s3');
-                });
-        }
+    //         // add POS
+    //         // dd($input); 
+    //     if ($input->hasFile('document')) {
+    //         $fileAdders = $newFinancingProposal->addMultipleMediaFromRequest(['document'])
+    //             ->each(function ($fileAdder) {
+    //                 $fileAdder->toMediaCollection('financingproposal','s3');
+    //             });
+    //     }
 
-            $items->push($newFinancingProposal);
+    //         $items->push($newFinancingProposal);
 
-       }
+    //    }
 
         return $this->sendResponse(FinancingProposalResource::collection($items), 'Financing Proposal saved successfully');
     }
