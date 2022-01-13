e82d297b654f0c1a6f45d2dd1eaa8635a148656a
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-13 18:28:59 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -72,10 +72,13 @@ class FinancingProposalAPIController extends AppBaseController
         // $proposal->financings()->detach();
         // $proposal->financings()->sync($inputs['financing_id']);
         // dd($inputs['checked']);
+        $result = ""
         if($inputs['checked'] === 'true'){
+            $result=$inputs['checked'];
             $proposal->financings()->syncWithoutDetaching($inputs['financing_id']);
         }
         if($inputs['checked'] === 'false'){
+            $result=$inputs['checked'];
             $deletedRows = FinancingProposal::where('proposal_id', $inputs['proposal_id'])->delete();
         }
         // $newFinancingProposal = $this->financingProposalRepository->create($inputs['financing_id']);
@@ -93,6 +96,7 @@ class FinancingProposalAPIController extends AppBaseController
     //    }
     // return($newFinancingProposal);
 
+    return($result);
         return $this->sendResponse(new ProposalResource($proposal), 'Financing Proposal saved successfully');
     }
 
