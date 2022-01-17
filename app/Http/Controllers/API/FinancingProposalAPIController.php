12bc1050afb4cfdd9d929f03ab17075f97dffb82
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-01-17 19:09:38 +0000

    Update FinancingProposalAPIController.php

--- app/Http/Controllers/API/FinancingProposalAPIController.php
+++ app/Http/Controllers/API/FinancingProposalAPIController.php
@@ -77,9 +77,11 @@ class FinancingProposalAPIController extends AppBaseController
         // dd (FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->exists());
         if(($inputs['checked'] === 'true') && (FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->exists() === true)){
             if ($request->hasFile('document')){
-                $fp = FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->first();
-                $fp->clearMediaCollection('financingproposal','s3');
-            }elseif(($inputs['checked'] === 'true') && (FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->exists() === false)){
+                $newFinancingProposal = FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->first();
+                $newFinancingProposal->clearMediaCollection('financingproposal','s3');
+            }
+            
+        if(($inputs['checked'] === 'true') && (FinancingProposal::where('proposal_id', $inputs['proposal_id'])->where('financing_id', $inputs['financing_id'])->exists() === false)){
                 // $proposal->financings()->syncWithoutDetaching($inputs['financing_id']);
                 $newFinancingProposal = $this->financingProposalRepository->create($inputs);
             }
