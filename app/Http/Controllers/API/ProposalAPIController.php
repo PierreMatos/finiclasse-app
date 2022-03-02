4d0ef66df96a51d0c1d627864fa623e47975f558
Gatts10 <orlando.carvalho31@gmail.com>
2022-03-02 15:15:05 +0000

    teste retake 15

--- app/Http/Controllers/API/ProposalAPIController.php
+++ app/Http/Controllers/API/ProposalAPIController.php
@@ -277,8 +277,10 @@ class ProposalAPIController extends AppBaseController
         }
 
         //Push Notification TradeIn
-        if ($proposal->tradein->state_id == 7) {
-            event(new PushAddTradeIn($proposal)); 
+        if ($proposal->tradein !== null) {
+            if ($proposal->tradein->state_id == 7) {
+                event(new PushAddTradeIn($proposal));
+            }
         }
 
         return $this->sendResponse(new ProposalResource($proposal), 'Proposal updated successfully');
