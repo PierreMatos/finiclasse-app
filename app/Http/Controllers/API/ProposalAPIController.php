ede30db2f88f6f7a7a1b7bbd5b8c7c96a8e0e888
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-03-29 15:44:23 +0100

    aceitar negocio se pendente

--- app/Http/Controllers/API/ProposalAPIController.php
+++ app/Http/Controllers/API/ProposalAPIController.php
@@ -507,6 +507,8 @@ class ProposalAPIController extends AppBaseController
                 $discPerc = ($desc / ($totalBenefits + $subTotal - ($ptl + $sigpu + $totalTransf))) * 100;
                 if ($discPerc < 0) {
                     $discPerc = 0;
+                }elseif($discPerc > 100){
+                    $discPerc = 100;
                 }
             } else {
 
@@ -559,8 +561,8 @@ class ProposalAPIController extends AppBaseController
                 // dd($discPerc);
                 if ($proposal->car->condition_id == 1) {
 
-                    if ($discPerc >= $min && $discPerc < $max && $authorization->id !== 6) {
-
+                    if ($discPerc >= $min && $discPerc <= $max && $authorization->id !== 6) {
+                        
                         if ($authorization->id !== 1) {
                             $proposal->state_id = 3;
                             // $proposal->save();
@@ -579,7 +581,7 @@ class ProposalAPIController extends AppBaseController
                 }
             }
 
-
+            
             if ($proposal->car->condition_id == 2 || $proposal->car->condition_id == 4) {
 
                 // $auth = $authorizations->find(6);
@@ -598,7 +600,9 @@ class ProposalAPIController extends AppBaseController
                     $proposal->initialBusinessStudy->business_study_authorization_id =  6;
                     $proposal->state_id = 3;
                     $proposal->save();
+
                 } elseif ($margin > 0) {
+
                     $business_study_authorization_id = 7;
                     $proposal->initialBusinessStudy->business_study_authorization_id =  7;
                     $proposal->state_id = 1;
