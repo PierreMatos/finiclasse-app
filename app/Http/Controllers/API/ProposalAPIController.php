d09ee969079955d1ffe559f5e6517a0b399cbdcf
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-04-07 20:45:26 +0100

    Update ProposalAPIController.php

--- app/Http/Controllers/API/ProposalAPIController.php
+++ app/Http/Controllers/API/ProposalAPIController.php
@@ -283,8 +283,12 @@ class ProposalAPIController extends AppBaseController
         }
 
         //Push Notification TradeIn
-        if ($proposal->tradein->state_id == 7) {
-            event(new PushAddTradeIn($proposal));
+        if (!IS_NULL($proposal->tradein)){
+
+            if ($proposal->tradein->state_id == 7) {
+                event(new PushAddTradeIn($proposal));
+            }
+            
         }
         
         return $this->sendResponse(new ProposalResource($proposal), 'Proposal updated successfully');
