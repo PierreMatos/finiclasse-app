ec7a53c13db0b495e0a318434b539957c01a3981
Gatts10 <orlando.carvalho31@gmail.com>
2022-04-11 12:43:38 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('quote:daily')
-            ->dailyAt('12:43')->weekdays()->timezone('Europe/Lisbon');
+            ->dailyAt('19:00')->weekdays()->timezone('Europe/Lisbon');
 
         $schedule->command('quote:weekly')
             ->weeklyOn(1, '9:00')->timezone('Europe/Lisbon');
