df95c9bbb8714422ae9df7555681eeacb5230ca1
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-04-11 16:49:27 +0100

    Update WebNotificationAPIController.php

--- app/Http/Controllers/API/WebNotificationAPIController.php
+++ app/Http/Controllers/API/WebNotificationAPIController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\AppBaseController;
+use App\Models\User;
+use App\Repositories\UserRepository;
 
 class WebNotificationAPIController extends AppBaseController
 {
-    public function __construct()
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepo)
     {
+        $this->userRepository = $userRepo;
         $this->middleware('auth');
     }
     
     public function storeToken(Request $request)
     {
-        auth()->user()->update(['device_key'=>$request->token]);
-        return response()->json(['Token successfully stored.']);
+        $user = $this->userRepository->find($request->user);
+
+        if ($user->device_key != $request->token ){
+
+            $user->update(['device_key'=>$request->token]);
+            return response()->json(['Token successfully stored.']);
+
+        }
     }
 }
