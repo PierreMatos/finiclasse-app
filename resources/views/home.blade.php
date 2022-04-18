20c9aa6f540429195b6afb29a80de8ebc8e1d6dd
Gatts10 <orlando.carvalho31@gmail.com>
2022-04-18 17:32:33 +0100

    teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Dashboard</h1>
+                    <h1>Dashboarddddddd</h1>
                 </div>
             </div>
         </div>
