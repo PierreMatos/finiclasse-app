c63bd15eb9fa20af6f02592c3e67f5e1dd9289fb
Gatts10 <orlando.carvalho31@gmail.com>
2022-05-24 16:27:00 +0100

    Revert "fix image x2"
    This reverts commit 925f26c5c34b1ed46a9b8aefaf8cdc20491bb4a3.

--- app/Http/Controllers/API/CarAPIController.php
+++ app/Http/Controllers/API/CarAPIController.php
@@ -167,16 +167,39 @@ class CarAPIController extends AppBaseController
         /** @var Car $car */
         $car = $this->carRepository->find($id);
 
-        // add images
-        if ($request->hasFile('image')) {
+        // //Apagar imagem antiga se for mudada
+        // if ($request->hasFile('image')) {
+        //     $car->clearMediaCollection('cars');
+        // }
+    
+        //Apagar POS antiga se for mudada
+        if ($request->hasFile('pos')) {
+            $car->clearMediaCollection('pos');
+        }
+
+        //Verificar se a imagem existe
+        // $file = $request->file('image');
+
+        //Verificar se a POS existe
+        // $file = $request->file('pos');
+
+        //adicionar imagem
+        if (empty($request->hasFile('image'))) {
+            //Passar a variable input sem colocar nova imagem
+            $input = $request->all();
+        } else {
+            //Actualizar imagem se colocar uma nova
+            $input = $request->all();
+
             $fileAdders = $car->addMultipleMediaFromRequest(['image'])
                 ->each(function ($fileAdder) {
                     $fileAdder->toMediaCollection('cars','s3');
                 });
         }
 
-        //Verificar se a imagem existe POS
         $file = $request->file('pos');
+
+        //Verificar se a imagem existe POS
         if($request->hasFile('file') == null) {
             //Passar a variable input sem colocar nova imagem
             $input = $request->all();
