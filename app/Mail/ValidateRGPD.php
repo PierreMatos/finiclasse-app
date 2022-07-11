c5f5ed9feaabe0bb474c9b78597002da0c0ec188
Gatts10 <orlando.carvalho31@gmail.com>
2022-07-11 17:36:44 +0100

    teste4

--- app/Mail/ValidateRGPD.php
+++ app/Mail/ValidateRGPD.php
@@ -32,7 +32,7 @@ class ValidateRGPD extends Mailable
     public function build()
     {
         return $this->from(env('MAIL_FROM_ADDRESS'), 'Finiclasse')
-            ->to('jfcarvalho@remotepartner.co')
+            ->to('franciscocarvalho88@gmail.com')
             ->bcc('support@aideal.app')
             ->subject('Validação RGPD')
             ->markdown('mail.validateRGPD');
