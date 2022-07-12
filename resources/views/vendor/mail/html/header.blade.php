3d1aa8c21b73b207ba127b9f23bf61f8c30253f2
Gatts10 <orlando.carvalho31@gmail.com>
2022-07-12 10:28:57 +0100

    test img

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header" style="width: 100%; height: 100%; background-color: #000; padding: 50px;">
 <a href="https://www.finiclasse.pt/" style="display: inline-block;">
 @if (trim($slot) === 'Finiclasse')
-<img src="{{ asset('storage/images/Logo.svg') }}" alt="Finiclasse Logo" style="width: 187px; height: 35px;">
+<img src="{{ URL::asset('storage/images/Logo.svg') }}" alt="Finiclasse Logo" style="width: 187px; height: 35px;">
 @else
 {{ $slot }}
 @endif
