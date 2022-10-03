84d089147184a6c3fe05fed77b39691ebb0da5d4
PierreMatos <60151514+PierreMatos@users.noreply.github.com>
2022-10-03 16:13:48 +0100

    acesso negado com login errado

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -470,6 +470,9 @@
                         @yield('message')
                     </p>
 
+                    @if (Auth::check())
+                        {{auth()->logout();}}
+                    @endif
                     <a href="{{ app('router')->has('home') ? route('home') : url('/') }}">
                         <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
                             {{ __('Go Home') }}
